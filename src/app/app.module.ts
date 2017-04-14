49f79e05087c17b7a216c540248b7722985f6b93
Pasha <pushkarpn@gmail.com>
2017-04-14 10:06:47 +0300

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,48 @@
-import { BrowserModule } from '@angular/platform-browser';
+import {Component} from '@angular/core';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
+import {RouterModule,Routes} from '@angular/router';
+import {LocationStrategy,HashLocationStrategy,APP_BASE_HREF} from '@angular/common';
 
 import { AppComponent } from './app.component';
+import {SearchComponent} from './components/SearchComponent';
+import {ArtistComponent} from './components/ArtistComponent';
+import {TrackComponent} from './components/TrackComponent';
+import {AlbumComponent} from './components/AlbumComponent';
+
+import {SPOTIFY_PROVIDERS} from './services/SpotifyService';
+
+
+const routes: Routes = [
+  { path: '', redirectTo: 'search', pathMatch: 'full' },
+  { path: 'search', component: SearchComponent },
+  { path: 'artists/:id', component: ArtistComponent },
+  { path: 'tracks/:id', component: TrackComponent },
+  { path: 'albums/:id', component: AlbumComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    ArtistComponent,
+    TrackComponent,
+    AlbumComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(routes) // <-- routes
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ AppComponent ],
+  providers: [
+    SPOTIFY_PROVIDERS,
+    {provide: APP_BASE_HREF, useValue: '/'},
+    {provide: LocationStrategy, useClass: HashLocationStrategy}
+  ]
 })
 export class AppModule { }
+
